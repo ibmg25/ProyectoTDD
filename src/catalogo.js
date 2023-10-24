84d7ea0e4810bb3eb5a4c70fc0e05b478e99b730
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-10-23 20:10:59 -0400

    muestra una lista con una kata

--- src/catalogo.js
+++ src/catalogo.js
@@ -1,5 +1,17 @@
+
 class Catalogo{
+
+  constructor() {
+    this.listakatas = []; 
+  }
+
+  insertarKata(kata){
+    this.listakatas.push(kata);
+  }
   getLista() {
+    if (this.listakatas.length>0){
+      return this.listakatas[0].printKata();
+    }
     return [];
   }
 }
--- src/plataformaTDD.test.js
+++ src/plataformaTDD.test.js
@@ -1,12 +1,30 @@
 import Catalogo from "./catalogo.js";
 
 
+class Kata{
+  printKata(){
+    return "<div><p>Título: Kata</p><p>Descripción: Descripción vacía</p><p>Categoría: Ninguna</p><p>Dificultad: Media</p></div>";
+  }
+}
 
 describe("Mostrar lista de katas", () => {
-  const catalogo = new Catalogo();
+
+  let catalogo;
+  beforeEach(() => {
+    catalogo = new Catalogo();
+  });
+
   it("deberia mostrar una lista de katas vacia", () => {
 
     expect(catalogo.getLista()).toEqual([]);
+  });
+
+  it("deberia mostrar una lista con una kata", () => {
+
+    const kata1 = new Kata();
+    catalogo.insertarKata(kata1)
+    const kataEsperada = "<div><p>Título: Kata</p><p>Descripción: Descripción vacía</p><p>Categoría: Ninguna</p><p>Dificultad: Media</p></div>"
+    expect(catalogo.getLista()).toEqual(kataEsperada);
   });
 });
 
