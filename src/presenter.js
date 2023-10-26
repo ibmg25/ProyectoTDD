345a31f803b9c8991020581e7316962d2c28c9f6
Guillermo <guichi.maldo@gmail.com>
2023-10-25 21:48:57 -0400

    refactoring presenter

--- src/presenter.js
+++ src/presenter.js
@@ -4,20 +4,17 @@ import Kata from "./kata.js";
 const Titulo = document.querySelector("#Titulo");
 const form = document.querySelector("#buscar-form");    
 const div = document.querySelector("#resultado-div");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const titulo = Titulo.value;
-  //const secondNumber = Number.parseInt(second.value);
-
- //div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
- });
-
+const titulo=null;
 const catalogo = new Catalogo();
 const kata1 = new Kata();
 const kata2 = new Kata("Kata2", "sin categoria", "Dificil", "nueva descripcion");
 catalogo.insertarKata(kata1);
 catalogo.insertarKata(kata2);
-
 div.innerHTML = catalogo.printLista();
+
+
+
+
+
+
+
