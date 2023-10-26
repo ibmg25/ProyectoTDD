3433e742f081106cc1a319d2e94434360414f8c3
Guillermo <guichi.maldo@gmail.com>
2023-10-25 22:02:53 -0400

    refactoring funcion buscar por titulo

--- src/presenter.js
+++ src/presenter.js
@@ -1,19 +1,26 @@
 import Catalogo from "./catalogo.js";
 import Kata from "./kata.js";
 
-const Titulo = document.querySelector("#Titulo");
-const form = document.querySelector("#buscar-form");    
-const div = document.querySelector("#resultado-div");
-const titulo=null;
+ const Titulo = document.querySelector("#Titulo");
+ const form = document.querySelector("#buscar-form");
+ const div = document.querySelector("#resultado-div");
+
 const catalogo = new Catalogo();
 const kata1 = new Kata();
-const kata2 = new Kata("Kata2", "sin categoria", "Dificil", "nueva descripcion");
+const kata2 = new Kata("Kata nueva", "sin categoria", "media", "nueva descripcion");
 catalogo.insertarKata(kata1);
 catalogo.insertarKata(kata2);
 div.innerHTML = catalogo.printLista();
 
+ form.addEventListener("submit", (event) => {
+   event.preventDefault();
 
-
+   const titulo = toString(Titulo.value);
+   const catalogo2 = new Catalogo();
+   const kataBuscada = new Kata();
+   catalogo2.insertarKata(kataBuscada);
+   div.innerHTML = catalogo2.printLista();
+ });
 
 
 
