72dc1426ff71463cffe1d78c05a364060e82e0dc
Arturo10021 <arturo.guzman@ucb.edu.bo>
2023-11-13 22:54:39 -0400

    segunda prueba filtrar por categoria

--- cypress/e2e/categoria.cy.js
+++ cypress/e2e/categoria.cy.js
@@ -3,11 +3,15 @@
 //debería mostrar una lista con katas de una categoría
 //debería mostrar una lista con katas de otra categoría
 
-describe("deberia seleccionar la categoria de Validacion y formularios y no mostrar nada ya que no hay ninguna kata de esa categoria", () => {
+describe("Filtrar por categorias", () => {
   it("deberia mostrar una lista vacía si no hay katas de una categoría", () => {
     cy.visit("http://localhost:1234");
     cy.get("#filtrar-categoria-kata").select("Validacion y formularios");
     cy.get("#resultado-div").should("be.empty");
   });
-}
-);
+  it("deberia mostrar una lista con katas de una categoría", () => {
+    cy.visit("http://localhost:1234");
+    cy.get("#filtrar-categoria-kata").select("Lógica y matematicas");
+    cy.get("#resultado-div").should("not.be.empty");
+  });
+});
