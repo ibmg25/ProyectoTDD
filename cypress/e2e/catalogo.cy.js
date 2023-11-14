17e1f34ed16ea28db4544605aba10dd234719030
Ignacio <ignacio.martinez@ucb.edu.bo>
2023-11-13 22:46:49 -0400

    prueba dificultad cypress

--- cypress/e2e/catalogo.cy.js
+++ cypress/e2e/catalogo.cy.js
@@ -1,17 +1,28 @@
 
 describe("Mostrar lista de katas", () => {
+  
   it("deberia mostrar una lista con varias katas", () => {
-    cy.visit("/ruta-de-tu-pagina"); // Ajusta la ruta según la estructura de tu aplicación
+    cy.visit("/"); // Ajusta la ruta según la estructura de tu aplicación
 
     cy.get("#resultado-div")
       .find(".kata")   // Busca elementos con la clase .kata dentro del div
       .should("have.length.greaterThan", 0); // Asegura que haya al menos un elemento .kata dentro del div
   });
+
   it("deberia mostrar la kata buscada por nombre", () => {
-    cy.visit("/ruta-de-tu-pagina"); // Ajusta la ruta según la estructura de tu aplicación
+    cy.visit("/"); // Ajusta la ruta según la estructura de tu aplicación
 
     cy.get("#resultado-div")
     cy.get("#titulo").type("Kata 2");
+    cy.get("#buscar-button").click() // Busca elementos con la clase .kata dentro del div
+    cy.should("have.length",1 ); // Asegura que haya al menos un elemento .kata dentro del div
+  });
+
+  it("deberia mostrar la kata buscada por dificultad", () => {
+    cy.visit("/"); // Ajusta la ruta según la estructura de tu aplicación
+
+    cy.get("#resultado-div")
+    cy.get("#filtrar-dificultad-kata").select("Facil");
     cy.get("#buscar-button").click() // Busca elementos con la clase .kata dentro del div
     cy.should("have.length",1 ); // Asegura que haya al menos un elemento .kata dentro del div
   });
