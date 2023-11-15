40e07ae9f56fe380dec051a742f00d15d092d707
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-11-14 22:56:51 -0400

    muestra la kata con los valores editados en la interfaz de usuario

--- cypress/e2e/operacionesKata.cy.js
+++ cypress/e2e/operacionesKata.cy.js
@@ -8,6 +8,18 @@ describe("Editar una kata del catalogo", () => {
       cy.get("#descripcion-kata-editar").should("have.value", "Descripcion vacia");
     });
  
+    it("deberia devolver la kata con los valores que quiero modificar", () => {
+        cy.visit("/");
+        cy.get('.boton-editar').eq(0).click();
+        cy.get("#titulo-kata-editar").clear().type("NuevoTitulo");
+        cy.get("#categoria-kata-editar").clear().type("Validacion y formularios");
+        cy.get("#dificultad-kata-editar").select("Facil");
+        cy.get("#descripcion-kata-editar").clear().type("Nueva descripcion");
+        cy.get("#titulo-kata-editar").should("have.value", "NuevoTitulo");
+        cy.get("#categoria-kata-editar").should("have.value", "Validacion y formularios");
+        cy.get("#dificultad-kata-editar").should("have.value", "Facil");
+        cy.get("#descripcion-kata-editar").should("have.value", "Nueva descripcion");
+      });
   });
   
 //   describe("Editar una kata del catalogo", () => {
