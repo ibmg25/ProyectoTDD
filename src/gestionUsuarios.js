2d0beae5c780b4348ff65fb41ac73807cd202e6e
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-11-19 21:04:43 -0400

    Devuelve un mensaje de bienvenida con el nombre del usuario al registrar usuario

--- src/gestionUsuarios.js
+++ src/gestionUsuarios.js
@@ -1,6 +1,6 @@
 class GestionUsuarios{
     registrarUsuario(nombreDeUsuario, contrasenia){
-        return "¡Bienvenido usuario! Tu registro se ha completado con éxito.";
+        return "¡Bienvenido " + nombreDeUsuario +"! Tu registro se ha completado con éxito.";
     }
 }
 export default GestionUsuarios;
--- src/operacionesUsuario.test.js
+++ src/operacionesUsuario.test.js
@@ -12,4 +12,8 @@ describe("Registrar con usuario y contraseña", () => {
         expect(gestionUsuarios.registrarUsuario("usuario", "contrasenia")).toEqual(mensajeEsperado);
     });
 
+    it("deberia devolverme un mensaje de bienvenida con el nombre del usuario", () => {
+        const mensajeEsperado = "¡Bienvenido user123! Tu registro se ha completado con éxito.";
+        expect(gestionUsuarios.registrarUsuario("user123", "contrasenia")).toEqual(mensajeEsperado);
+    });
 });
